751f25f2635eebfbd1a5aee78314cdc7f86b0826
steve <redsox333@gmail.com>
2014-03-11 01:06:51 -0700

    Adding schedule comparison page.  Could refactor view to make it shorter.

--- band_scheduler/views.py
+++ band_scheduler/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
@@ -37,7 +37,7 @@ def mySchedule(request):
     context = {}
     if user.is_authenticated():
         try:
-            bands = Schedule.objects.get(user=user).bands.all().order_by('day', 'name')
+            bands = getUserBands(user)
             context['bands'] = bandsByDay(bands)
         except Schedule.DoesNotExist:
             pass
@@ -52,18 +52,70 @@ def userSchedule(request, username):
     }
     try:
         user = User.objects.get(username=username)
-        bands = Schedule.objects.get(user=user).bands.all().order_by('day', 'name')
+        bands = getUserBands(user)
         context['bands'] = bandsByDay(bands)
     except (User.DoesNotExist, Schedule.DoesNotExist):
         pass
     return render(request, 'band_scheduler/user-schedule.html', context)
 
-def bandsByDay(band_list):
-    bands_list = []
+def compareSchedules(request):
+    usernames = request.GET.get('schedules').split(',')
+    users = [request.user]
+    user_bands = {}
+    user_bands[request.user.username] = bandsByDay(getUserBands(request.user), True)
+
+    # get each user's schedule
+    for username in usernames:
+        user = get_object_or_404(User, username=username)
+        users.append(user)
+        user_bands[username] = bandsByDay(getUserBands(user), True)
+    bands_in_common = {}
+    days = Band.DAY_CHOICES
+
+    # find common bands by day
+    for day in days:
+        day = day[0]
+
+        # filter for common bands amongst all users
+        for user in users:
+            name = user.username
+            if day not in bands_in_common:
+                bands_in_common[day] = set(user_bands[name][day])
+            else:
+                bands_in_common[day] = bands_in_common[day] & set(user_bands[name][day])
+
+        # remove common bands from individual users
+        for user in users:
+            name = user.username
+            user_bands[name][day] = list(set(user_bands[name][day]) - bands_in_common[day])
+
+        # turn values into a list
+        bands_in_common[day] = list(bands_in_common[day])
+
+    context = {
+        'users': users,
+        'user_bands': user_bands,
+        'bands_in_common': bands_in_common,
+        'days': days
+    }
+    return render(request, 'band_scheduler/compare.html', context)
+
+# helpers
+def bandsByDay(band_list, dict_form=False):
+    if dict_form:
+        bands = {}
+    else:
+        bands = []
     for day in Band.DAY_CHOICES:
         i = 0
         limit = len(band_list)
         while i < limit and band_list[i].day == day[0]: i += 1
-        bands_list.append(band_list[:i])
+        if dict_form:
+            bands[day[0]] = band_list[:i]
+        else:
+            bands.append(band_list[:i])
         band_list = band_list[i:]
-    return bands_list
+    return bands
+
+def getUserBands(user):
+    return Schedule.objects.get(user=user).bands.all().order_by('day', 'name')
